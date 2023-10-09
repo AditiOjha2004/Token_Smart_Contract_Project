10224f4b3305d9d016b60787a88d1349bd2a31ec
AditiOjha2004 <147323126+AditiOjha2004@users.noreply.github.com>
2023-10-09 21:28:18 +0530

    Update token.sol

--- token.sol
+++ token.sol
@@ -1,1 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
 
+contract MyToken {
+    string public tokenName = "Aditi";
+    string public tokenSymbol = "abc";
+    uint256 public totalSupply;
+    mapping(address => uint256) public balances;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    constructor() {
+        totalSupply = 0;
+    }
+
+    function mint(address _to, uint256 _value) external {
+        require(_to != address(0), "Invalid address");
+        totalSupply += _value;
+        balances[_to] += _value;
+        emit Transfer(address(0), _to, _value);
+    }
+
+    function burn(uint256 _value) external {
+        require(balances[msg.sender] >= _value, "Insufficient balance");
+        totalSupply -= _value;
+        balances[msg.sender] -= _value;
+        emit Transfer(msg.sender, address(0), _value);
+    }
+
+    function transfer(address _to, uint256 _value) external returns (bool) {
+        require(_to != address(0), "Invalid address");
+        require(balances[msg.sender] >= _value, "Insufficient balance");
+
+        balances[msg.sender] -= _value;
+        balances[_to] += _value;
+
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function getInfo() external view returns (string memory, string memory) {
+        return (tokenName, tokenSymbol);
+    }
+}
